19cb7a75b32bf7d38a506388b4fb758a8ca617e1
Sharifjonov Xamidullo <sharifjonovxamidullo6677@gmail.com>
2025-08-15 13:42:49 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // static export qilish
-  distDir: 'dist',  // build natijasini dist papkaga yozadi
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: "/my-contact-form", // bu repo nomi bilan bir xil bo'lishi kerak
 };
 
 export default nextConfig;
